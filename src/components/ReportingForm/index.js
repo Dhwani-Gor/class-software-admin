0df339d3abae4fa9223677dd47d4ac0df570de24
pooja-bagaria <pbagariya@technithunder.com>
2026-02-25 12:46:00 +0530

    add journal type id

--- src/components/ReportingForm/index.js
+++ src/components/ReportingForm/index.js
@@ -96,7 +96,7 @@ const ReportingForm = () => {
     const payload = {
       clientId: selectedShip.id,
       anniversaryDate: selectedDate,
-      journalId: journalId,
+      journalId: selectedReportNumber.journalTypeId,
     };
 
     await updateAnniversaryByClient(payload);
